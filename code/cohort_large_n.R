76acfefa35c656ac026c6409957a48f408269b8f
wieandk <wieandk@chop.edu>
2024-06-07 15:45:06 -0400

    pivot large n table and add implementation for vs/vc check

--- code/cohort_large_n.R
+++ code/cohort_large_n.R
@@ -6,6 +6,7 @@
 #'                  should be added
 #' @param num_col the numeric column to be used to compute the statistics
 #' @param grp_vars grouping variables for the computation
+#' @param check_string string indicating the check type
 #' @param time a logical indicating whether the check is time dependent
 #'
 #' @return a dataframe with summary statistics for the numeric column
@@ -14,36 +15,74 @@
 #'         if time = `TRUE`, the mean and median are returned.
 #'         
 #'         if time = `FALSE`, the max, min, q1, q3, mean, and median are returned 
+#'         
+#'         These values are treated as "sites" to assist with Shiny implementation
 #' 
 summarize_large_n <- function(dq_output,
                               num_col,
                               grp_vars,
+                              check_string,
                               time = FALSE){
   
   if(!time){
     
-    summ_dat <- dq_output %>%
-      group_by(!!!syms(grp_vars)) %>%
-      summarise(max_val = max(!!sym(num_col)),
-                min_val = min(!!sym(num_col)),
-                q1 = quantile(!!sym(num_col), 0.25),
-                q3 = quantile(!!sym(num_col), 0.75),
-                mean_val = mean(!!sym(num_col)),
-                median_val = median(!!sym(num_col)))
-    
-    final_dat <- dq_output %>%
-      left_join(summ_dat)
+    if(check_string == 'vc' || check_string == 'vs'){
+      denoms <- dq_output %>% distinct(check_type, table_application, 
+                                       total_denom_ct) %>%
+        group_by(table_application, check_type) %>%
+        summarise(allsite_denom = sum(total_denom_ct))
+      
+      newprops <- dq_output %>%
+        distinct(table_application, measurement_column, 
+                 vocabulary_id, total_denom_ct, tot_ct, 
+                 check_type) %>%
+        group_by(check_type, table_application, measurement_column, vocabulary_id) %>%
+        summarise(allsite_ct = sum(tot_ct)) %>%
+        left_join(denoms) %>%
+        mutate(allsite_prop = round(allsite_ct / allsite_denom, 3)) %>%
+        pivot_longer(cols = allsite_prop,
+                     names_to = 'site',
+                     values_to = 'tot_prop') %>% select(-c(allsite_ct, allsite_denom)) %>%
+        collect()
+      
+      final_dat <- dq_output %>%
+        collect() %>%
+        bind_rows(newprops)
+      
+      
+    }else{
+      summ_dat <- dq_output %>%
+        group_by(!!!syms(grp_vars)) %>%
+        summarise(max_val = max(!!sym(num_col)),
+                  min_val = min(!!sym(num_col)),
+                  q1 = quantile(!!sym(num_col), 0.25),
+                  q3 = quantile(!!sym(num_col), 0.75),
+                  mean_val = mean(!!sym(num_col)),
+                  median_val = median(!!sym(num_col))) %>%
+        pivot_longer(cols = c(max_val, min_val, q1, q3, mean_val, median_val),
+                     names_to = 'site',
+                     values_to = num_col) %>% collect()
+      
+      final_dat <- dq_output %>%
+        collect() %>% 
+        bind_rows(summ_dat)
+    }
     
   }else{
     
     summ_dat <- dq_output %>%
       group_by(!!!syms(grp_vars)) %>%
       summarise(mean_val = mean(!!sym(num_col)),
-                median_val = median(!!sym(num_col)))
+                median_val = median(!!sym(num_col))) %>%
+      pivot_longer(cols = c(mean_val, median_val),
+                   names_to = 'site',
+                   values_to = num_col) %>% collect()
     
     final_dat <- dq_output %>%
-      left_join(summ_dat)
+      collect() %>%
+      bind_rows(summ_dat)
   }
   
+  return(final_dat)
   
 }
--- code/driver_large_n.R
+++ code/driver_large_n.R
@@ -6,53 +6,59 @@
   
   rslt$dc_ln <- summarize_large_n(dq_output = results_tbl('dc_output_pp') %>%
                                     filter(site != 'total'),
+                                  check_string = 'dc',
                                   num_col = 'plot_prop',
                                   grp_vars = c('domain', 'check_name', 'application'))
   
   output_tbl(rslt$dc_ln %>% union(results_tbl('dc_output_pp') %>%
-                                    filter(site == 'total')), 'dc_output_ln')
+                                    filter(site == 'total') %>% collect()), 'dc_output_ln')
   
   ## Vocabulary & Valueset Conformance
   
   rslt$vcvs_ln <- summarize_large_n(dq_output = results_tbl('vc_vs_output_pp') %>%
                                       filter(site != 'total'),
+                                    check_string = 'vc',
                                     num_col = 'tot_prop',
                                     grp_vars = c('table_application', 'measurement_column',
                                                  'check_type', 'check_name'))
   
   output_tbl(rslt$vcvs_ln %>% union(results_tbl('vc_vs_output_pp') %>%
-                                    filter(site == 'total')), 'vc_vs_output_ln')
+                                    filter(site == 'total') %>% collect()), 'vc_vs_output_ln')
   
   ## Unmapped Concepts
   
   rslt$uc_ln <- summarize_large_n(dq_output = results_tbl('uc_output_pp') %>% 
                                     filter(site != 'total'),
+                                  check_string = 'uc',
                                   num_col = 'unmapped_prop',
                                   grp_vars = c('measure', 'check_name'))
   
   output_tbl(rslt$uc_ln %>% union(results_tbl('uc_output_pp') %>%
-                                      filter(site == 'total')), 'uc_output_ln')
+                                      filter(site == 'total') %>% collect()), 'uc_output_ln')
   
   rslt$uc_by_yr_ln <- summarize_large_n(dq_output = results_tbl('uc_by_year_pp') %>%
                                           filter(site != 'total'),
+                                        check_string = 'uc',
                                         num_col = 'prop_total',
                                         grp_vars = c('year_date', 'unmapped_description',
                                                      'check_name'),
                                         time = TRUE)
   
   output_tbl(rslt$uc_by_yr_ln %>% union(results_tbl('uc_by_year_pp') %>%
-                                      filter(site == 'total')), 'uc_by_year_ln')
+                                      filter(site == 'total') %>% collect()), 'uc_by_year_ln')
   
   ## Person Facts
   
   rslt$pf_person_ln <- summarize_large_n(dq_output = results_tbl('pf_output_pp') %>%
                                            filter(site != 'total'),
+                                         check_string = 'pf',
                                          num_col = 'fact_pts_prop',
                                          grp_vars = c('check_description', 'check_name',
                                                       'visit_type'))
   
   rslt$pf_visit_ln <- summarize_large_n(dq_output = results_tbl('pf_output_pp') %>%
                                            filter(site != 'total'),
+                                        check_string = 'pf',
                                          num_col = 'fact_visits_prop',
                                          grp_vars = c('check_description', 'check_name',
                                                       'visit_type'))
@@ -61,10 +67,9 @@
   joincols <- joincols[!joincols %in% c('max_val', 'min_val', 'q1', 'q3', 
                                         'mean_val', 'median_val')]
   
-  rslt$pf_final_ln <- rslt$pf_person_ln %>% left_join(rslt$pf_visit_ln,
-                                                      by = joincols,
-                                                      suffix = c('_pt', '_visits')) %>%
-    union(results_tbl('pf_output_pp') %>% filter(site == 'total'))
+  rslt$pf_final_ln <- rslt$pf_person_ln %>% select(-fact_visits_prop) %>% 
+    left_join(rslt$pf_visit_ln %>% select(-fact_pts_prop)) %>%
+    union(results_tbl('pf_output_pp') %>% filter(site == 'total') %>% collect())
   
   output_tbl(rslt$pf_final_ln, 'pf_output_ln')
   
@@ -72,42 +77,47 @@
   
   rslt$bmc_ln <- summarize_large_n(dq_output = results_tbl('bmc_gen_output_pp') %>%
                                      filter(site != 'total'),
+                                   check_string = 'bmc',
                                    num_col = 'best_row_prop',
                                    grp_vars = c('check_name', 'check_desc'))
   
   output_tbl(rslt$bmc_ln%>% union(results_tbl('bmc_gen_output_pp') %>%
-                                          filter(site == 'total')), 'bmc_gen_output_ln')
+                                          filter(site == 'total') %>% collect()), 
+             'bmc_gen_output_ln')
   
   ## Domain Concordance
   
   rslt$dcon_ln <- summarize_large_n(dq_output = results_tbl('dcon_output_pp') %>%
                                             filter(site != 'total'),
+                                    check_string = 'dcon',
                                           num_col = 'prop',
                                           grp_vars = c('cohort', 'check_name', 
                                                        'check_type'))
   
   output_tbl(rslt$dcon_ln %>% union(results_tbl('dcon_output_pp') %>%
-                                    filter(site == 'total')), 'dcon_output_ln')
+                                    filter(site == 'total') %>% collect()), 'dcon_output_ln')
   
   ## MF VisitID
   
   rslt$mf_visitid_ln <- summarize_large_n(dq_output = results_tbl('mf_visitid_pp') %>%
                                             filter(site != 'total'),
+                                          check_string = 'mf',
                                           num_col = 'prop_missing_visits_total',
                                           grp_vars = c('measure', 'domain', 'check_name'))
   
   output_tbl(rslt$mf_visitid_ln %>% union(results_tbl('mf_visitid_pp') %>%
-                                      filter(site == 'total')), 'mf_visitid_ln')
+                                      filter(site == 'total') %>% collect()), 'mf_visitid_ln')
   
   ## Expected Concepts Present
   
   rslt$ecp_ln <- summarize_large_n(dq_output = results_tbl('ecp_output_pp') %>%
                                      filter(site != 'total'),
+                                   check_string = 'ecp',
                                    num_col = 'prop_with_concept',
                                    grp_vars = c('concept_group', 'check_name'))
   
   output_tbl(rslt$ecp_ln %>% union(results_tbl('ecp_output_pp') %>%
-                                            filter(site == 'total')), 'ecp_output_ln')
+                                            filter(site == 'total') %>% collect()), 'ecp_output_ln')
   
   ## Facts Over Time
   
